b6aa01a015f6c097764a96441d137c90678ff5ba
HCDUNG-02\Admin <hcdung2109@gmail.com>
2022-05-10 20:14:26 +0700

    up

--- bai3/get.php
+++ bai3/get.php
@@ -1,18 +1,27 @@
+<?php
+    if (isset($_GET['fname'])) {
+        $fname = $_GET['fname'];
+        print_r($fname);
+    }
+
+    if (isset($_GET['lname'])) {
+        $lname = $_GET['lname'];
+        print_r($lname);
+    }
+
+?>
 <!DOCTYPE html>
 <html>
 <body>
 
-<h2>HTML Forms</h2>
+<h2>HTML Forms GET</h2>
 
-<form action="/action_page.php">
-    <label for="fname">First name:</label><br>
-    <input type="text" id="fname" name="fname" value="John"><br>
-    <label for="lname">Last name:</label><br>
-    <input type="text" id="lname" name="lname" value="Doe"><br><br>
+<form action="" method="GET">
+    <label for="fname">Username:</label><br>
+    <input type="text" id="fname" name="username" value=""><br>
+    <label for="lname">Password:</label><br>
+    <input type="text" id="lname" name="password" value=""><br><br>
     <input type="submit" value="Submit">
 </form>
-
-<p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
-
 </body>
 </html>
